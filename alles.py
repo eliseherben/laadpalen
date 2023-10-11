362abb014570b93aad5cf630aeb7866a3d8414d2
eliseherben <146279872+eliseherben@users.noreply.github.com>
2023-10-11 16:22:22 +0200

    Update alles.py

--- alles.py
+++ alles.py
@@ -310,7 +310,7 @@ with tab3:
     import plotly.express as px
     import numpy as np
     import plotly.graph_objects as go
-    import seaborn as sns
+    # import seaborn as sns
     import matplotlib.pyplot as plt
     from plotly.subplots import make_subplots
     import math
